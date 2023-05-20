451f783b50e0d2456f61814006e784335114a811
daxida <80173028+daxida@users.noreply.github.com>
2023-05-20 23:44:34 +0200

    examples css & some js syntax

--- script.js
+++ script.js
@@ -7,21 +7,22 @@ let CODE_ARR = null;
 file_read("perl_query.pl");
 
 async function file_read(file) {
- READER.open("get", file, true); 
- READER.onreadystatechange = () => {
-   if (READER.readyState == 4) {
-     QUERY = READER.responseText; 
-   }
- };
- READER.send(null);
+  READER.open("get", file, true); 
+  READER.onreadystatechange = () => {
+    if (READER.readyState == 4) {
+      QUERY = READER.responseText; 
+    }
+  };
+  READER.send(null);
 }
 
-
 async function code_arr_text(code_arr) {
+  // Parses code_arr into displayable html
+
   let ind = 0;
   return code_arr.map(x => {
     const [i, code, description, example_arr] = x;
-    let z = `
+    let entry_html = `
 <table id = "filter-${i}" class = "filter-aaa" border="5">
 <tr>
   <td class="code code-x" id = "code-${ind}"><xmp>${code}</xmp></td>
@@ -34,59 +35,73 @@ async function code_arr_text(code_arr) {
 `;
     ind += 1;
     let example_dropdown = ""
+
     if (typeof example_arr !== "undefined") {
-      example_dropdown = example_arr.map(e => {
-        let stdin = "";
-        let c = "";
-        let stdout = "";
-        // console.log(e);
-        if (e.length == 3) {
-          [stdin, c, stdout] = e;
-          stdin = `<td class="snippet">${stdin}</td>`;
-          stdout = `<td class="snippet">${stdout}</td>`
-        } else if (e.length == 2) {
-          // stdin = "";
-          [c, stdout] = e;
-          stdout = `<td class="snippet">${stdout}</td>`
+      example_dropdown = example_arr.map(ex => {
+        let [stdin, ex_code, stdout] = ["None", "", ""];
+        console.log(ex);
+        
+        const sz = ex.length
+        if (sz == 3) {
+          [stdin, ex_code, stdout] = ex;
+        } else if (sz == 2) {
+          [ex_code, stdout] = ex;
+        } else if (sz == 1) {
+          ex_code = ex;
         } else {
-          [c] = e;
+          throw new Error("Unimplemented (Example of unknown size)")
         }
 
-        // c = c.replaceAll("🦋", "");
-        let n = `<tr class="tr-example">
-  ${stdin}
-  <td class="snippet code-x" id = "code-${ind}"><xmp>${c}</xmp></td>
-  ${stdout}
-</tr>`;
-        ind += 1;
-        return n;
+        const example_html = `
+<tr class="tr-example">
+  <tr>
+    <td class="snippet code-x" id="code-${ind}"><xmp>${ex_code}</xmp></td>
+    <td>
+      <table>
+        <tr>
+          <td class="snippet-input">I: ${stdin}</td>
+        </tr>
+        <tr>
+          <td class="snippet-output">O: ${stdout}</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</tr>`
+
+        console.log(example_html)
+
+        ind += 1; 
+        return example_html;
       }).join("\n");
     }
 
-    z += `<table id = "filter-${i}" class="table-example dropdown ${i}">${example_dropdown}</table>`;
+    entry_html += `<table id = "filter-${i}" class="table-example dropdown ${i}">${example_dropdown}</table>`;
 
-    return z;
+    return entry_html;
 
   }).join("\n")
 }
 
 async function code_mirror_rep() {
-   [...document.getElementsByClassName("code-x")].sort((a, b) => a.id.split("-")[1] - b.id.split("-")[1]).map(elem => { 
-     CodeMirror.runMode(elem.innerText, "text/x-perl", elem)
-   });
+   [...document.getElementsByClassName("code-x")]
+     .sort((a, b) => a.id.split("-")[1] - b.id.split("-")[1])
+     .map(elem => { CodeMirror.runMode(elem.innerText, "text/x-perl", elem) });
 }
 
 async function file_parse() {
+  // Parses QUERY into CODE_ARR
+
   const QUERY_ARR = QUERY.split("\n***\n");
 // console.log(QUERY_ARR);
   CODE_ARR = QUERY_ARR.map((query, ind) => {
-    console.log(query);
-    let [code, desc_pre] = query.split("\n---\n");
-    let [desc, ...example_str_arr] = desc_pre.split("\niii\n");
-    let example_arr = example_str_arr.map(example_str => {let [stdin, code, stdout] = example_str.split(/\n?(?:ccc|ooo)\n/g); 
-
-      console.log("stdout", stdout);
+    // console.log(query);
+    const [code, desc_pre] = query.split("\n---\n");
+    const [desc, ...example_str_arr] = desc_pre.split("\niii\n");
+    let example_arr = example_str_arr.map(example_str => {
+      const [stdin, code, stdout] = example_str.split(/\n?(?:ccc|ooo)\n/g); 
       let arr = [];
+      
       if (stdin !== "") {
         arr.push(stdin);
       }
@@ -99,26 +114,18 @@ async function file_parse() {
 
       return arr;
     });
-    return example_arr.length == 0 ? [ind, code, desc] : [ind, code, desc, example_arr];
+    
+    let ret = [ind, code, desc];
+    if (example_arr.length > 0) ret.push(example_arr);
+
+    return ret;
   });
 
-  console.log("code arr ", CODE_ARR);
+  // console.log("code arr ", CODE_ARR);
   main(CODE_ARR);
-}
-async function file_wait() {
-  let FILE_READ_LOOP = setInterval(function() {
-    if (QUERY === null) {
-      return;
-    }
-
-    clearInterval(FILE_READ_LOOP);
-    file_parse();
-  }, 10);
-
 }
 
 async function main(code_arr) {
-  // IND = 0;
   let a = await code_arr_text(code_arr);
   // console.log(a);
   await $("#TABLE").html(a);
@@ -134,6 +141,15 @@ function toggle_search() {
   }
 }
 
+async function file_wait() {
+  let FILE_READ_LOOP = setInterval(function() {
+    if (QUERY === null) return;
+
+    clearInterval(FILE_READ_LOOP);
+    file_parse();
+  }, 10);
+}
+
 $(document).ready(() => {
   file_wait();
 
@@ -144,8 +160,8 @@ $(document).ready(() => {
     }
     // console.log("HI");
     let query = $("#QUERY").val();
-    console.log("query", '[' + query + ']');
-    console.log("HI ", CODE_ARR);
+    // console.log("query", '[' + query + ']');
+    // console.log("HI ", CODE_ARR);
 
     $(".dropdown").map((_i, drop) => {
       drop.classList.remove("show");
@@ -182,17 +198,17 @@ $(document).ready(() => {
       })
 
     } else {
-      $(".filter-aaa").map((_i, query_entry) => {
-        query_entry.style.display = "none";
-      });
+      $(".filter-aaa").hide();
 
       $(".table-example").map((_i, query_example_tab) => {
+        query_example_tab.style.display = "table";
+
+        // console.log(_i, query_example_tab)
         if (query_example_tab.children.length > 0) {
           let query_example = query_example_tab.children[0].children;
           // console.log("what ", children);
           let query_match = 0;
           for(let i = 0; i < query_example.length; i++){
-
             if (query_example[i].innerText.includes(query)) {
               query_match += 1;
               query_example[i].style.display = "table-row";
@@ -200,15 +216,7 @@ $(document).ready(() => {
               query_example[i].style.display = "none";
             }
           }
-
-          if (query_match) {
-            query_example_tab.style.display = "table";
-          } else {
-
-            query_example_tab.style.display = "none";
-          }
-        } else {
-          query_example_tab.style.display = "none";
+          if (query_match) query_example_tab.style.display = "table";
         }
       });
     }
